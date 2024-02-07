836f4d112b7a22c6446c77e9b5740e21cb7e6618
Herman Menor <herman.menor@gmail.com>
2024-02-07 22:25:37 +0200

    Change URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-portfolio-uzair.vercel.app",
+  site: "https://hmenorjr.github.io/jhazmagne.d.tobias",
   integrations: [
     tailwind(),
     sitemap({
