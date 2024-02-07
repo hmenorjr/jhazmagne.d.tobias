85970138f178d0ce85d3d044b037bd84efa72f90
Herman Menor <herman.menor@gmail.com>
2024-02-07 22:32:35 +0200

    Add / to site i astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://hmenorjr.github.io/",
-  base: "jhazmagne.d.tobias"
+  base: "/jhazmagne.d.tobias"
   integrations: [
     tailwind(),
     sitemap({
