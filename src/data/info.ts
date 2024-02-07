c5c3e9100fad084d4bd23aedde9d4c005df1ae25
Herman Menor <herman.menor@gmail.com>
2024-02-07 21:15:13 +0000

    Update baseUrl, name and about

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,15 +1,8 @@
 export const info = {
-  baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
-  jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  baseUrl: "https://hmenorjr.github.io",
+  name: "Jhazmagne Dhane D. Tobias",
+  // jobDescription: "IT-utvecklare",
+  about: `Jag är bra på organisera många saker. Jag kan också städer upp litet rum, tvätta tallrikar och bestick. Jag tycker om dator och teknologi. Jag vill ska bli en IT-profiesionell eller dator-utvecklare.
   `,
 
   experience: [
